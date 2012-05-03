8bb6a199a1fcec4f945575e51298108cecafeb32
yewton <yewton@yewton-NB100-H.(none)>
2012-05-04 03:48:59 +0900

    ex8

--- sec4/ex8.cpp
+++ sec4/ex8.cpp
@@ -14,52 +14,26 @@
  * マス目の数を計算するプログラムを作成せよ
  */
 
-typedef int (*func)(int, int);
-
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
-int divide(int a, int b) { return a / b; }
-int mul(int a, int b) { return a * b; }
-
-static map<string, int> str2num;
-
-void init(map<char, pair<string, func> > &mapChar) {
-    mapChar['+'] = pair<string, func>("sum", &add);
-    mapChar['-'] = pair<string, func>("sub", &sub);
-    mapChar['/'] = pair<string, func>("div", &divide);
-    mapChar['*'] = pair<string, func>("mul", &mul);
-
-    str2num["1"] = str2num["one"]   = 1;
-    str2num["2"] = str2num["two"]   = 2;
-    str2num["3"] = str2num["three"] = 3;
-    str2num["4"] = str2num["four"]  = 4;
-    str2num["5"] = str2num["five"]  = 5;
-    str2num["6"] = str2num["six"]   = 6;
-    str2num["7"] = str2num["seven"] = 7;
-    str2num["8"] = str2num["eight"] = 8;
-    str2num["9"] = str2num["nine"]  = 9;
-}
-
 int main() {
-    string a, b;
-    char op;
-    map<char, pair<string, func> > mapChar;
-    pair<string, func> sf;
-    init(mapChar);
-
-    cin >> a >> b >> op;
-
-    if (mapChar.find(op) == mapChar.end()) {
-        cout << "Invalid operator: " << op << endl;
-        return 1;
+    double sum = 0;
+    bool a = true,
+        b = true,
+        c = true;
+    for (int n = 1, i = 1; i <= 32; ++i, n*=2)
+    {
+        sum += n;
+        if (a && 1000 <= sum) {
+            a = false;
+            cout << "1000 over: " << i << "masu(" << sum << " grane)" << endl;
+        }
+        if (b && 1000000 <= sum) {
+            b = false;
+            cout << "1000000 over: " << i << "masu(" << sum << " grane)" << endl;
+        }
+        if (c && 1000000000 <= sum) {
+            c = false;
+            cout << "1000000000 over: " << i << "masu(" << sum << " grane)" << endl;
+        }
     }
-    sf = mapChar[op];
-    if (str2num.find(a) == str2num.end() ||
-        str2num.find(b) == str2num.end()) {
-        cout << "Invalid operand: " << a << ", " << b << endl;
-        return 1;
-    }
-    cout << "The " << sf.first << " of " << a << " and " << b << " is "
-         << (*(sf.second))(str2num[a], str2num[b]) << endl;
     return 0;
 }
